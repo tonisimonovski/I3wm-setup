10fb57faa60a2db6a20f2c126f94d1d8f4743566
tonisimonovski <149823950+tonisimonovski@users.noreply.github.com>
2024-04-19 01:33:20 +0200

    Update i3wm_setup.sh

--- i3wm_setup.sh
+++ i3wm_setup.sh
@@ -1,21 +1,5 @@
 #!/usr/bin/env bash
 
-# Check if Script is Run as Root
-if [[ $EUID -ne 0 ]]; then
-  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
-  exit 1
-fi
-
-username=$(id -u -n 1000)
-builddir=$(pwd)
-
-# Update packages list and update system
-apt update -y 
-apt upgrade -y
-
-# Install nala
-apt install nala -y
-
 # All in one install script i3WM on Debian minimal
 # install i3wm with --aditional pack
 # addet for ricing the theme
@@ -29,30 +13,75 @@ apt install nala -y
 # Download Playerctl:
 # https://github.com/acrisci/playerctl/...
 
-# Install Rhthmbox: 
-# sudo apt install rhythmbox
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./install.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
 
 # Install packages after installing base Debian with no GUI
+
+# Update packages list 
+apt update -y 
+
+# and update system
+apt upgrade -y
+
+# Install nala
+apt install nala -y
+
+# Install Rhthmbox: 
+# sudo apt install rhythmbox
 
 # xorg display server installation
 sudo apt install -y xorg xserver-xorg xbacklight xbindkeys xinput
 
-# build-essential
+# build-essential If you do not plan to build Debian packages, you don't need this package. 
+# Starting with dpkg (>= 1.14.18) this package is required for building Debian packages. 
 sudo apt install -y build-essential
 
 # xdg-user-dirs-update updates the current state of the users user-dirs.dir.
+# xdg-user-dirs is a tool to help manage "well known" user directories like the desktop folder and the music folder.
 xdg-user-dirs-update
 mkdir ~/Oldconfig
 
 # Tools/System
-sudo apt install -y dialog mtools dosfstools acpi acpid gvfs-backends xfce4-power-manager avahi-daemon lxappearance neofetch
-sudo systemctl enable avahi-daemon
-sudo systemctl enable acpid
+# dialog -  Displays user-friendly dialog boxes from shell scripts 
+# mtolls - Tools for manipulating MSDOS files 
+# dosfstools - utilities for making and checking MS-DOS FAT filesystems 
+# acpi - displays information on ACPI devices 
+# acpid - Advanced Configuration and Power Interface event daemon 
+# gvfs-backedns -	userspace virtual filesystem
+# xfce4-power-manager - power manager for Xfce desktop
+
+# avahi-daemon - Avahi is a system which facilitates service discovery 
+# on a local network via the mDNS/DNS-SD protocol suite.
+
+# lxappearance - 	LXDE GTK+ theme switcher
+# neofetch - Shows Linux System Information with Distribution Logo 
+# btop - Modern and colorful command line resource monitor that shows usage and stats 
+
+
+sudo apt install -y dialog mtools dosfstools acpi acpid gvfs-backends xfce4-power-manager 
+
+# sudo apt install -y avahi-daemon 
+
+sudo apt install lxappearance neofetch btop
+
+#sudo systemctl enable avahi-daemon
+#sudo systemctl enable acpid
 
 # Network 
+# network-manager - network management framework (daemon and userspace tools) 
+# network-manager-gnome - network management framework (GNOME frontend) 
 sudo apt install -y network-manager network-manager-gnome
 
-# Thunar
+
+# Thunar and accessories
 sudo apt install -y thunar thunar-archive-plugin thunar-volman file-roller
 
 # Terminal (eg. terminator,kitty,xfce4-terminal)
@@ -64,17 +93,28 @@ sudo apt install -y pulseaudio alsa-utils pavucontrol volumeicon-alsa pamixer
 # Browser Installation (eg. chromium, brave, firefox-esr, librewolf)
 sudo apt install -y firefox-esr
 
-# Desktop background browser/handler 
-# feh --bg-fill /path/to/directory 
+# Desktop background browser/handler (feh --bg-fill /path/to/directory )
+
 # sudo apt install -y nitrogen 
 sudo apt install -y feh
 
+### Installing fonts
 # Fonts and icons for now
-sudo apt install -y fonts-roboto fonts-recommended fonts-ubuntu fonts-font-awesome fonts-terminus papirus-icon-theme
+sudo apt install -y fonts-roboto 
+sudo apt install -y fonts-recommended 
+sudo apt install -y fonts-noto-color-emoji 
+sudo apt install -y fonts-ubuntu 
+sudo apt install -y fonts-font-awesome 
+sudo apt install -y fonts-terminus 
+
+# Papirus theme
+sudo apt install -y papirus-icon-theme
+# Install Papirus icon theme
+wget -qO- https://git.io/papirus-icon-theme-install | sh
 
 # EXA installation
 # replace ls command in .bashrc file with line below
-# alias ls='exa -al --long --header --color=always --group-directories-first' 
+# alias ls=exa -al --long --header --color=always --group-directories-first 
 sudo apt install -y exa
 
 
@@ -103,37 +143,19 @@ sudo apt install xclip -y
 # materia-gtk-theme --Materia. Materia is a Material Design theme for GNOME/GTK based desktop environments.
 sudo apt install materia-gtk-theme -y
 
-# lxappearance --It's a desktop-independent theme switcher for GTK+
-sudo apt install lxappearance -y
-
 # flameshot --Flameshot is a free and open-source, cross-platform tool to take screenshots
 sudo apt install flameshot -y
 
 # pnmixer --PNMixer is a simple mixer application designed to run in your system tray.
 sudo apt install pnmixer -y
    
-# Install Papirus icon theme
-wget -qO- https://git.io/papirus-icon-theme-install | sh
-
-# Installing Essential Programs 
-
-# picom 
-sudo install picom -y
-
-# thunar 
-sudo install thunar -y
-
-# nitrogen 
-sudo install nitrogen -y
+### Installing Essential Programs 
 
 # lxpolkit 
 sudo install lxpolkit -y
 
 # x11-xserver-utils 
 sudo install x11-xserver-utils -y
-
-# unzip 
-sudo install unzip -y 
 
 # wget 
 sudo install wget -y
@@ -143,12 +165,6 @@ sudo install pipewire -y
 
 # wireplumber 
 sudo install wireplumber -y
-
-# pavucontrol 
-sudo install pavucontrol -y
-
-# build-essential 
-sudo install build-essential -y
 
 # libx11-dev 
 sudo install libx11-dev -y
@@ -170,56 +186,29 @@ sudo install zoxide -y
 
 # Installing Other less important Programs
 
-nala install neofetch flameshot psmisc mangohud papirus-icon-theme fonts-noto-color-emoji lightdm -y
-
-# Download Nordic Theme
-#cd /usr/share/themes/
-#git clone https://github.com/EliverLara/Nordic.git
-
-# Download Font Awesome and the cheat sheet for it
-# Font Awesome downloads: https://github.com/FortAwesome/Font-Awesome
-# Font Awesome cheat sheet: https://fontawesome.com/v3/cheatsheet/
-
-# Installing fonts
-cd $builddir 
-nala install fonts-font-awesome -y
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
-chown $username:$username /home/$username/.fonts/*
-# Reloading Font
-fc-cache -vf
-# Removing zip Files
-rm ./FiraCode.zip ./Meslo.zip
-
-# Install Nordzy cursor
-git clone https://github.com/alvatip/Nordzy-cursors
-cd Nordzy-cursors
-./install.sh
-cd $builddir
-rm -rf Nordzy-cursors
+nala install -y psmisc 
+nala install -y mangohud 
+nala install -y lightdm -y
 
 # Enable graphical login and change target from CLI to GUI
 systemctl enable lightdm
 systemctl set-default graphical.target
+sudo systemctl enable lightdm
+
 
 # Enable wireplumber audio service
 sudo -u $username systemctl --user enable wireplumber.service
 
-
-# Use nala
-bash scripts/usenala
-
-
-# Geany Text Editor, text editor, markdown editor
-# sudo apt install -y geany 
-# sudo apt install -y geany-plugin-addons geany-plugin-git-changebar geany-plugin-overview geany-plugin-spellcheck geany-plugin-treebrowser geany-plugin-vimode
-# sudo apt install -y geany-plugins \ # all plugins
-# sudo apt install -y mousepad ghostwriter
-# sudo apt install -y l3afpad
-
-# My Favorites
 # Multimedia/GFX
-sudo apt install -y mpv qimgv scrot gimp obs-studio mkvtoolnix-gui redshift eog brightnessctl
+sudo apt install -y mpv 
+sudo apt install -y qimgv 
+sudo apt install -y scrot 
+sudo apt install -y gimp 
+sudo apt install -y obs-studio 
+sudo apt install -y mkvtoolnix-gui 
+sudo apt install -y redshift 
+sudo apt install -y eog 
+sudo apt install -y brightnessctl
 
 # PDF 
 sudo apt install -y  evince pdfarranger
@@ -228,19 +217,15 @@ sudo apt install -y  evince pdfarranger
 sudo apt install -y numlockx 
 sudo apt install -y figlet 
 sudo apt install -y galculator
-sudo apt install -ycpu-x
+sudo apt install -y cpu-x
 sudo apt install -y udns-utils
 sudo apt install -y whois 
 sudo apt install -y curl 
 sudo apt install -y tree
 
-# Install Lightdm Console Display Manager
-# sudo apt install -y lightdm lightdm-gtk-greeter-settings
-# sudo systemctl enable lightdm
-
 ########################################################
-# End of script 
-#
+# End of script #
+
 
 sudo apt autoremove
 
